a194bfaad9844d6cff4f1f556f021ffc90206e76
T6751 <66636084+T6751@users.noreply.github.com>
2023-02-12 10:30:25 +0300

    фикс дубликат кеев в ямле для чеинжлога (#96)

--- tgui/packages/tgui/interfaces/Changelog.js
+++ tgui/packages/tgui/interfaces/Changelog.js
@@ -39,6 +39,27 @@ const icons = {
   unknown: { icon: 'info-circle', color: 'label' },
 };
 
+export function fixYAMLDuplicateKeys(yaml_text) {
+  let yaml_fix = {};
+  let currentKey = '';
+  for (let line of yaml_text.split('\n')) {
+    if (!line.match(/^[^\s]*:$/)) {
+      yaml_fix[currentKey].push(line);
+    } else {
+      currentKey = line;
+      if (!yaml_fix[currentKey]) {
+        yaml_fix[currentKey] = [];
+      }
+    }
+  }
+  let yaml_fixed_text = '';
+  for (let key of Object.keys(yaml_fix).sort()) {
+    yaml_fixed_text += key + '\n';
+    yaml_fixed_text += yaml_fix[key].join('\n') + '\n';
+  }
+  return yaml_fixed_text;
+}
+
 export class Changelog extends Component {
   constructor() {
     super();
@@ -88,7 +109,7 @@ export class Changelog extends Component {
             self.getData(date, attemptNumber + 1);
           }, timeout);
         } else {
-          self.setData(yaml.load(result, { schema: yaml.CORE_SCHEMA }));
+          self.setData(yaml.load(fixYAMLDuplicateKeys(result), { schema: yaml.CORE_SCHEMA }));
         }
       });
   };
