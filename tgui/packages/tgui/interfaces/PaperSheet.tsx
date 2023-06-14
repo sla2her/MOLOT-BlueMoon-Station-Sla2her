0bee5fe11471c7a8a3ad14f9472646716a8aba15
Марк <chuykov.andrey.2001@gmail.com>
2023-06-15 00:53:36 +0500

    Не осилил.

--- tgui/packages/tgui/interfaces/PaperSheet.tsx
+++ tgui/packages/tgui/interfaces/PaperSheet.tsx
@@ -112,7 +112,7 @@ const canEdit = (heldItemDetails?: WritingImplement): boolean => {
 const fieldRegex: RegExp = /\[((?:_+))\]/gi;
 
 // Handles the ghost stamp when attempting to stamp paper sheets.
-export class PaperSheetStamper extends Component<PaperSheetStamperProps> {
+class PaperSheetStamper extends Component<PaperSheetStamperProps> {
   style: null;
   state: PaperSheetStamperState = { x: 0, y: 0, rotation: 0, yOffset: 0 };
   scrollableRef: RefObject<HTMLDivElement>;
